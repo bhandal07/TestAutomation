137410c8bef2253f4e93c8ef506085e78ddf007e
bhandal <bhandal07@gmail.com>
2021-05-09 05:03:56 +0000

    test new gfile

---
+++ src/test/testpush.java
@@ -0,0 +1,29 @@
+package test;
+
+
+import java.util.Scanner;
+
+public class testpush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Number;
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        Number = scanner.nextInt();
+        if(Number % 2 == 0)
+        {
+            System.out.println("The given number "+Number+" is Even ");
+        }
+        else
+        	
+        {
+            System.out.println("The given number "+Number+" is not even ");
+	}
+		
+		
+
+	}
+
+}
+
